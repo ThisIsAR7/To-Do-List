2afd0c895c7c47ffe18a8518b84756b313d54050
AR7 | Arvin Rezaei <158085547+ThisIsAR7@users.noreply.github.com>
2024-09-06 16:08:39 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function addTask(){
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === 'LI'){
-        e.target.classList.toggle("");
+        e.target.classList.toggle("checked");
         saveData()
     }
     else if(e.target.tagName === "SPAN"){
