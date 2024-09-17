93e17b6f990433f770b5618892a8de5e908e07e9
AR7 | Arvin Rezaei <158085547+ThisIsAR7@users.noreply.github.com>
2024-09-17 23:41:15 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask(){
-    if(inputBox. === ''){
+    if(inputBox.value === ''){
         alert("You Must Write Something!");
     }
     else{
