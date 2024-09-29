5bc8a24686457a3c4f3c0ab2750969dfbc8eb5be
AR7 | Arvin Rezaei <158085547+ThisIsAR7@users.noreply.github.com>
2024-09-29 20:53:39 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+const listContainer = document.("list-container");
 
 function addTask(){
     if(inputBox.value === ''){
